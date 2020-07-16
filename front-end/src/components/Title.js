c2401579a09b6539c6ed876c3abc5a02a3220e21
Hannah Tuttle <hannahtuttle28@gmail.com>
2020-07-15 18:58:04 -0700

    added title component, added friendlist

---
+++ front-end/src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Title = () => {
+
+    return (
+        <div>
+            <h1>Hangman</h1>
+            <div>{/*use for css code/animation for hangman*/}</div>
+        </div>
+    )
+}
+
+export default Title;
