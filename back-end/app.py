2788dd38beaf36b2d2b433935980f5cb0f12b6f1
Hannah Tuttle <hannahtuttle28@gmail.com>
2020-07-18 11:08:31 -0700

    added python-dotenv, added schema models in models.py

--- back-end/app.py
+++ back-end/app.py
@@ -1,5 +1,14 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+print(os.environ.get('APP_SETTINGS'))
 app = Flask(__name__)
+app.config.from_object(os.environ.get('APP_SETTINGS'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
 
 
 @app.route('/')
