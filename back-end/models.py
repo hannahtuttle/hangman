5c69d72f5c24f1be35ad2131101863f48f6e837d
Hannah Tuttle <hannahtuttle28@gmail.com>
2020-07-19 14:47:08 -0700

    added db manipulation logic to endpoints

--- back-end/models.py
+++ back-end/models.py
@@ -1,5 +1,5 @@
 from app import db
-from sqlalchemy.dialects.postgresql import JSON
+# from sqlalchemy.dialects.postgresql import JSON
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -9,9 +9,9 @@ class User(db.Model):
     password = db.Column(db.String())
     # game = db.relationship('Game', backref = 'player', lazy='dynamic')
 
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+    # def __init__(self, username, password):
+    #     self.username = username
+    #     self.password = password
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
@@ -23,9 +23,9 @@ class Phrase(db.Model):
     phrase = db.Column(db.String())
     # game = db.relationship('Game', backref = 'phrase', lazy='dynamic')
 
-    def __init__(self, phrase, game):
-       self.phrase = phrase
-       self.game = game
+    # def __init__(self, phrase, game):
+    #    self.phrase = phrase
+    #    self.game = game
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
@@ -39,11 +39,11 @@ class Game(db.Model):
     phrase_id_1 = db.Column(db.Integer, db.ForeignKey('phrases.id'))
     phrase_id_2 = db.Column(db.Integer, db.ForeignKey('phrases.id'))
 
-    def __init__(self, player_1, player_2, phrase_id_1, phrase_id_2):
-       self.player_1 = player_1
-       self.player_2 = player_2
-       self.phrase_id_1 = phrase_id_1
-       self.phrase_id_2 = phrase_id_2
+    # def __init__(self, player_1, player_2, phrase_id_1, phrase_id_2):
+    #    self.player_1 = player_1
+    #    self.player_2 = player_2
+    #    self.phrase_id_1 = phrase_id_1
+    #    self.phrase_id_2 = phrase_id_2
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
