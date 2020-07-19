12b5a9054d36fb6b4863643bb4024ec223671463
Hannah Tuttle <hannahtuttle28@gmail.com>
2020-07-18 20:55:12 -0700

    set up sign_up route, working through import bug

--- models.py
+++ models.py
@@ -7,6 +7,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String())
     password = db.Column(db.String())
+    # game = db.relationship('Game', backref = 'player', lazy='dynamic')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
@@ -16,6 +21,11 @@ class Phrase(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     phrase = db.Column(db.String())
+    # game = db.relationship('Game', backref = 'phrase', lazy='dynamic')
+
+    def __init__(self, phrase, game):
+       self.phrase = phrase
+       self.game = game
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
@@ -28,6 +38,12 @@ class Game(db.Model):
     player_2 = db.Column(db.Integer, db.ForeignKey('users.id'))
     phrase_id_1 = db.Column(db.Integer, db.ForeignKey('phrases.id'))
     phrase_id_2 = db.Column(db.Integer, db.ForeignKey('phrases.id'))
+
+    def __init__(self, player_1, player_2, phrase_id_1, phrase_id_2):
+       self.player_1 = player_1
+       self.player_2 = player_2
+       self.phrase_id_1 = phrase_id_1
+       self.phrase_id_2 = phrase_id_2
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
